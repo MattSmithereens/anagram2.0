44f6b44322e2761839c4f7878cf9906c9aa4ae6c
Kevin Ahn and Scott Fraser <kevin@>
2018-06-27 11:39:07 -0700

    tests and intial work

--- Anagram.Tests/Models.Tests/UnitTest1.cs
+++ Anagram.Tests/Models.Tests/UnitTest1.cs
@@ -1,4 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using Anagram;
 
 namespace Anagram.Tests
 {
@@ -6,8 +8,32 @@ namespace Anagram.Tests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void GetSetUserWords_GetsSetsUserWords_StringList()
         {
+            AnagramProgram TestAnagram = new AnagramProgram();
+            string testWord = "test";
+            TestAnagram.SetWordOne(testWord);
+            Assert.AreEqual(testWord, TestAnagram.GetWordOne());
+        }
+
+        [TestMethod]
+        public void GetSetListWords_GetsSetsListWords_StringList()
+        {
+            AnagramProgram TestAnagram = new AnagramProgram();
+            string testWords = "test test test";
+            TestAnagram.SetListOfWords(testWords);
+            Assert.AreEqual(testWords, TestAnagram.GetListOfWords()[0]);
+        }
+
+        [TestMethod]
+        public void CompileList_WillCompileList_StringList()
+        {
+            AnagramProgram TestAnagram = new AnagramProgram();
+            List<string> testList = new List<string>(new string[] { "a", "b", "c" });
+            string word = "a b c";
+            string[] inputList = word.Split(' ');
+            TestAnagram.CompileList(inputList);
+            CollectionAssert.AreEqual(testList, TestAnagram.GetListOfWords());
         }
     }
 }
